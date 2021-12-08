8e2c540005262ed272d43ec95dab7506b1766c72
Joe Naples <jnaples90@gmail.com>
2021-12-08 16:17:27 -0500

    Added Gallery

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,8 @@ import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { baseUrl } from "../shared/baseUrl";
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from "expo-media-library";
 
 class LoginTab extends Component {
    constructor(props) {
@@ -161,7 +163,43 @@ class RegisterTab extends Component {
          });
          if (!capturedImage.cancelled) {
             console.log(capturedImage);
-            this.setState({ imageUrl: capturedImage.uri });
+            this.processImage(capturedImage.uri);
+         }
+      }
+   };
+
+   processImage = async (imgUri) => {
+      const processedImage = await ImageManipulator.manipulateAsync(
+         imgUri,
+         [
+            {
+               resize: {
+                  width: 400,
+               },
+            },
+         ],
+         {
+            format: ImageManipulator.SaveFormat.PNG,
+         }
+      );
+
+      console.log("test", processedImage);
+      MediaLibrary.saveToLibraryAsync(newImg.uri);
+      this.setState({ imageUrl: processedImage.uri });
+   };
+
+   getImageFromGallery = async () => {
+      const cameraRollPermission = await Permissions.askAsync(
+         Permissions.CAMERA_ROLL
+      );
+      if (cameraRollPermission.status === "granted") {
+         const capturedImage = await ImagePicker.launchImageLibraryAsync({
+            allowsEditing: true,
+            aspect: [1, 1],
+         });
+         if (!capturedImage.cancelled) {
+            console.log("hey", capturedImage);
+            this.processedImage(capturedImage.uri);
          }
       }
    };
@@ -194,6 +232,7 @@ class RegisterTab extends Component {
                      style={styles.image}
                   />
                   <Button title='Camera' onPress={this.getImageFromCamera} />
+                  <Button title='Gallery' onPress={this.getImageFromGallery} />
                </View>
                <Input
                   placeholder='Username'
